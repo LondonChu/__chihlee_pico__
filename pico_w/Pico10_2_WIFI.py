bf7d496ef5c9df074ff25927f43f2f1dfcb594b6
LondonChu <kyz87786@gmail.com>
2023-12-16 16:18:50 +0800

    wifi

--- pico_w/Pico10_2_WIFI.py
+++ pico_w/Pico10_2_WIFI.py
@@ -1,4 +1,26 @@
 import network
 import time
 
-wlan= network.WLAN(net)
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.disconnect()
+#wlan.connect('Robert_iPhone','0926656')
+wlan.connect('LondonChu','78787878')
+
+#等待連線或失敗
+#status = 0,1,2 正在連線
+#status = 3 連線成功
+#<0,>3失敗的連線
+
+max_wait = 40
+while max_wait>0:
+    status = wlan.status()
+    if status < 0 or status >=3:
+        break
+    max_wait-=1
+    print("等待連線...")
+    time.sleep(1)
+    
+print(wlan.ifconfig())
+print(status)
+print(wlan.isconnected())
