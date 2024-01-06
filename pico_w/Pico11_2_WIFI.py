4ed35afb916c0ca514b443e34c720fce1ceacc23
LondonChu <kyz87786@gmail.com>
2024-01-06 16:30:51 +0800

    Update Pico11_2_WIFI.py

--- pico_w/Pico11_2_WIFI.py
+++ pico_w/Pico11_2_WIFI.py
@@ -1,12 +1,14 @@
 import network
 import time
-from machine import WDT
+from machine import WDT,Timer,ADC,RTC
+import urequests as requests
+
 
 def connect():
     # enable station interface and connect to WiFi access point
     nic = network.WLAN(network.STA_IF)
     nic.active(True)
-    nic.connect('LondonChu','78787878')
+    nic.connect('LondonChu','7878787')
 
     max_wait = 10
 
@@ -31,5 +33,47 @@ def connect():
     else:
         print("成功連線")
         print(nic.ifconfig())
+        
+
+
+
+
+def alert(temperature):
+    print('要爆炸了!')
+    #import RTC
+    rtc = RTC()
+    date_tuple=rtc.datetime()
+    year =date_tuple[0]
+    mounth =date_tuple[1]
+    day =date_tuple[2]
+    hour =date_tuple[4]
+    minites =date_tuple[5]
+    second =date_tuple[6]
+    date_str =f'{year}-{mounth}-{day} {hour}:{minites}:{second}'
+    response = requests.get(f'https://hook.us1.make.com/vc90h2cjk8u4h9bxkd6wx3wmzhhcdjdw?Address=Pico&Date={date_str}&Temperature={temperature}')
+    print(help(response))
+    response.close()
+    
+def callback1(t:Timer):
+    global start
+    sensor = ADC(4)    
+    vol = sensor.read_u16() * (3.3/65535)
+    temperature = 27 - (vol-0.706) / 0.001721
+    print(f'溫度:{temperature}')    
+    delta = time.ticks_diff(time.ticks_ms(), start)
+    print(delta)
+    #溫度超過24度,並且發送alert()的時間已經大於60秒
+    if temperature >= 24 and delta >= 60 * 1000:        
+        alert(temperature)
+        start = time.ticks_ms()#重新設定計時的時間
+        
 
 connect()
+
+start = time.ticks_ms() - 60 * 1000 #應用程式啟動時,計時時間,先減60秒    
+time1 = Timer()
+time1.init(period=1000,callback=callback1)
+
+
+
+
