69f2d56fb82393023b34b8c51ae080aa0dbeb8d7
LondonChu <95125901+LondonChu@users.noreply.github.com>
2024-01-20 16:19:42 +0800

    update

--- pico_w/Pico13_1.py
+++ pico_w/Pico13_1.py
@@ -18,9 +18,29 @@ btn = Pin(14,mode=Pin.PULL_DOWN)
 #    time.sleep_ms(500)
 
 #方法二
+#while True:
+#    if btn.value():
+#        red_led.value(1)
+#    else:
+#        red_led.value(0)
+
+
+#switch button
+#解決彈跳
+is_press = False
+led_status = False
+
 while True:
     if btn.value():
-        red_led.value(1)
-    else:
-        red_led.value(0)
+        time.sleep_ms(50)
+        if btn.value():
+            is_press = True
+    elif is_press:
+        time.sleep_ms(50)
+        if btn.value() == False:
+            print('release')
+            led_status = not led_status        
+            red_led.value(led_status)        
+            is_press = False
+            
     
