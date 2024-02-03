384096655e7c3f63e3170bc33beb427a13756bb4
LondonChu <95125901+LondonChu@users.noreply.github.com>
2024-02-03 03:57:35 +0000

    update

--- GUI/main.py
+++ GUI/main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import time as t
 df = pd.DataFrame({
   'first column': [1, 2, 3, 4],
   'second column': [10, 20, 30, 50]
@@ -8,5 +9,5 @@ df = pd.DataFrame({
 df
 
 st.title("Pico_W_職能發展協會專案")
-st.header("雞舍:red[溫度]和:blue[光線]狀態")
+st.header(f"雞舍:red[溫度]和:blue[光線]狀態 :green[{t.localtime}]")
 st.divider()
