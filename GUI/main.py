6d8fc0e69e2f27bb87082edb19e665bd5f7c4977
LondonChu <95125901+LondonChu@users.noreply.github.com>
2024-02-03 05:06:27 +0000

    update

--- GUI/main.py
+++ GUI/main.py
@@ -1,15 +1,13 @@
 import streamlit as st
 import pandas as pd
-import time as t
+import datetime as t
 df = pd.DataFrame({
   'first column': [1, 2, 3, 4],
   'second column': [10, 20, 30, 50]
 })
-
 df
 
 st.title("Pico_W_職能發展協會專案")
-st.header(f"雞舍:red[溫度]和:blue[光線]狀態 :green[{t.localtime().tm_yday}]")
+st.header("雞舍:red[溫度]和:blue[光線]狀態")
+st.write(f":green[t.datetime.now()]")
 st.divider()
-
-print(t.localtime())
