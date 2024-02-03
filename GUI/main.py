e0f6d61fb3fe956ebca662b9516b75c426cbc586
LondonChu <95125901+LondonChu@users.noreply.github.com>
2024-02-03 08:06:33 +0000

    update

--- GUI/main.py
+++ GUI/main.py
@@ -2,9 +2,13 @@ import streamlit as st
 import pandas as pd
 import requests
 from streamlit_autorefresh import st_autorefresh
+from dotenv import load_dotenv
+import os
 
 import datetime as t
 import pytz
+
+load_dotenv('.env')
 
 st_autorefresh(interval=5000)
 
@@ -14,7 +18,7 @@ taiwan_tz = pytz.timezone("Asia/Taipei") #時區
 st.write(f":green[{t.datetime.now(taiwan_tz)}]")
 st.divider()
 
-url = 'https://blynk.cloud/external/api/get?token=MOPYimQu2tyQj136eu3jLO01iDMIeuOR&v0&v1'
+url = f'https://blynk.cloud/external/api/get?token={os.environ["API_KEY"]}&v0&v1'
 
 response = requests.request("GET",url)
 if response.status_code == 200:
