c9e1c23050f15febb57cc307dff1b0e021e3a500
LondonChu <95125901+LondonChu@users.noreply.github.com>
2024-02-03 05:07:28 +0000

    update

--- GUI/main.py
+++ GUI/main.py
@@ -1,13 +1,14 @@
 import streamlit as st
 import pandas as pd
 import datetime as t
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 50]
-})
-df
 
 st.title("Pico_W_職能發展協會專案")
 st.header("雞舍:red[溫度]和:blue[光線]狀態")
 st.write(f":green[{t.datetime.now()}]")
 st.divider()
+
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 50]
+})
+df
