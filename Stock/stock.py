d25313caac3c1044011ec5abe1574d52e36f09f8
LondonChu <95125901+LondonChu@users.noreply.github.com>
2024-02-06 10:00:15 +0000

    STOCK

--- Stock/stock.py
+++ Stock/stock.py
@@ -6,7 +6,7 @@ import pandas as pd
 import numpy as np
 import yfinance as yf
 
-st.title("女神股市監控室")
+st.title("C#")
 taiwan_tz = pytz.timezone("Asia/Taipei") #時區
 st.write(f":green[{t.datetime.now(taiwan_tz)}]")
 st.divider()
@@ -18,38 +18,49 @@ def calculate(df):
     df['-DM_DFF'] = -df['Low'].diff()
     df['+DM'] = np.where((df['+DM_DFF'] > df['-DM_DFF']) & (df['+DM_DFF'] > 0), df['+DM_DFF'], 0)#PDM
     df['-DM'] = np.where((df['-DM_DFF'] > df['+DM_DFF']) & (df['-DM_DFF'] > 0), df['-DM_DFF'], 0)#NDM    
-    df['ATR'] = df['TR'].rolling(window=14).sum()
-    df['+ADM'] = df['+DM'].rolling(window=14).sum()
-    df['-ADM'] = df['-DM'].rolling(window=14).sum()
-    # df['ATR'] = df['TR'].shift(1)*13/14+df['TR']*1/14
-    # df['+ADM'] = df['+DM'].shift(1)*13/14+df['+DM']*1/14
-    # df['-ADM'] = df['-DM'].shift(1)*13/14+df['-DM']*1/14
+    
+    # df['ATR'] = df['TR'].rolling(window=14).sum()/14
+    # df['+ADM'] = df['+DM'].rolling(window=14).sum()/14
+    # df['-ADM'] = df['-DM'].rolling(window=14).sum()/14
+
+    df['FTR'] = df['TR'].rolling(window=14).sum()/14
+    df['+FDM'] = df['+DM'].rolling(window=14).sum()/14
+    df['-FDM'] = df['-DM'].rolling(window=14).sum()/14
+
+    df['ATR'] = df['FTR'].shift(1)*13/14+df['TR']*1/14
+    df['+ADM'] = df['+FDM'].shift(1)*13/14+df['+DM']*1/14
+    df['-ADM'] = df['-FDM'].shift(1)*13/14+df['-DM']*1/14
+
     df['+DI'] = 100 * df['+ADM'] / df['ATR']
     df['-DI'] = 100 * df['-ADM'] / df['ATR']
-    #df['+DI'] = df['+DI'].shift(1)*13/14+df['+DI']*1/14
-    #df['-DI'] = df['-DI'].shift(1)*13/14+df['-DI']*1/14
+
     df['DX'] = 100 * abs(df['+DI'] - df['-DI']) / (df['+DI'] + df['-DI'])
-    df['ADX'] = (df['DX'].rolling(window=14).sum())/14
+
+    df['FDX'] = df['DX'].rolling(window=14).sum()/14
+
+    df['ADX'] = df['FDX'].shift(1)*13/14+df['DX']*1/14
+
+    #df['ADX'] = (df['DX'].rolling(window=14).sum())/14
     return df
 
 # 輸入股票代號
 stock_id = "3715.TW"
 # 抓取半年資料
-end = t.date(2022,10,4)#t.date.today()  # 資料結束時間
-start = t.date(2022,8,24)#end - t.timedelta(days=13)  # 資料起始時間
+end = t.date.today()  # 資料結束時間
+start = t.date(2022,8,25) #end - t.timedelta(days=100)  # 資料起始時間
 df = yf.download(stock_id, start=start, end=end).reset_index()  # 抓取股價資料
 df = calculate(df)
 df1 = pd.DataFrame({
   '日期': pd.to_datetime(df['Date']).dt.strftime('%Y/%m/%d'),
-  '開': df['Open'],
-  '高': df['High'],
-  '低': df['Low'],
-  '收': df['Close'],
+  'Open': df['Open'],
+  'High': df['High'],
+  'Low': df['Low'],
+  'Close': df['Close'],
   # '成交量': (df['Volume']/1000).round(0),
   # '+DM_DFF': df['+DM_DFF'],
   # '-DM_DFF': df['-DM_DFF'],
-  # '+DM': df['+DM'],
-  # '-DM': df['-DM'],
+  '+DM': df['+DM'],
+  '-DM': df['-DM'],
   # '+ADM':df['+ADM'],
   # '-ADM': df['-ADM'],
   # 'TR':df['TR'],
@@ -68,6 +79,6 @@ df2 = pd.DataFrame({
   '-DI': (df['-DI']).round(2),
   'ADX': df['ADX']
 })
-st.line_chart(df2,x='Date')
-st.balloons()
+#st.line_chart(df2,x='Date')
+#st.balloons()
 
